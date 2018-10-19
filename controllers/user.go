d8454191bf0d69166a71a2edbfc9118a2d80916b
lexville <alekikangethe@gmail.com>
2018-10-19 06:20:01 +0300

    Add login and register views

--- controllers/user.go
+++ controllers/user.go
@@ -9,7 +9,9 @@ import (
 // any other piece of data needed by the user
 // controller
 type UserController struct {
-	HomeView *view.View
+	HomeView     *view.View
+	LoginView    *view.View
+	RegisterView *view.View
 }
 
 // AddViewTemplates is responsible for getting all
@@ -20,6 +22,12 @@ func AddViewTemplates() *UserController {
 		HomeView: view.AddTemplateFiles(
 			"base",
 			"templates/welcome.gohtml"),
+		LoginView: view.AddTemplateFiles(
+			"base",
+			"templates/user/login.gohtml"),
+		RegisterView: view.AddTemplateFiles(
+			"base",
+			"templates/user/register.gohtml"),
 	}
 }
 
@@ -29,4 +37,18 @@ func AddViewTemplates() *UserController {
 // GET /
 func (uc *UserController) Get(w http.ResponseWriter, r *http.Request) {
 	uc.HomeView.Render(w, nil)
+}
+
+// Login is responsible for showing the resister view
+//
+// GET /login
+func (uc *UserController) Login(w http.ResponseWriter, r *http.Request) {
+	uc.LoginView.Render(w, nil)
+}
+
+// Register is responsible for showing the resister view
+//
+// GET /register
+func (uc *UserController) Register(w http.ResponseWriter, r *http.Request) {
+	uc.RegisterView.Render(w, nil)
 }
--- routes/routes.go
+++ routes/routes.go
@@ -14,6 +14,9 @@ func InitRoutes() {
 	homeController := controllers.AddViewTemplates()
 	r := mux.NewRouter()
 	r.HandleFunc("/", homeController.Get).Methods("GET")
+	r.HandleFunc("/register", homeController.Register).Methods("GET")
+	r.HandleFunc("/login", homeController.Login).Methods("GET")
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("public"))))
 	if err := http.ListenAndServe(":3000", r); err != nil {
 		log.Fatal("Unable to serve: ", err)
 	}
